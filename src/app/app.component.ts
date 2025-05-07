74f71b3f933bbc8cad726efc1d0ec4abbf18a81d
nirmalDhapodkar <nirmaldh759@gmail.com>
2025-05-07 06:26:33 +0530

    Made Some Components
    made handleClickEvent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Nirmal Dhapodkar';
-  name='Storm Valet';
+
+  handleClickEvent(){
+    alert('Function Called');
+    this.otherFunction();
+  }
+
+  otherFunction(){
+    console.log('Hello');
+  }
 }
